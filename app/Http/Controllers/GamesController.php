338220c968df014fad05b54fe439721564016d33
Unknown <sp0613907@otc.edu>
2018-04-04 09:48:01 -0500

    editing navbar and deleting extra instance of partials

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class games extends Controller
+class GamesController extends Controller
 {
     /**
      * Display a listing of the resource.
